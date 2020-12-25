1b20f98b024437d7f1852585d5e126e5d151faa1
DongGu <god.donggu@gmail.com>
2020-12-25 20:44:53 +0900

    #6.1 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    // this.setState({movies:movies}) // state.movies: data.data.movices
-    // movies를 저장하고, isLoading을 false로 바꿔줌
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section class="container">
-        {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div class="movies">
-            {isLoading
-              ? "Loading..."
-              : movies.map((movie) => {
-                  console.log(movie);
-                  return (
-                    <Movie
-                      key={movie.id}
-                      id={movie.id}
-                      year={movie.year}
-                      title={movie.title}
-                      summary={movie.summary}
-                      poster={movie.medium_cover_image}
-                      genres={movie.genres}
-                    ></Movie>
-                  );
-                })}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
