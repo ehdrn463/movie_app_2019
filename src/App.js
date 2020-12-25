df036f71698f94e969b5c6459d1e35520e2bdb31
DongGu <god.donggu@gmail.com>
2020-12-25 17:00:37 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,48 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
+  // Component 생성될 때 바로 시작되는 메소드
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false, book: true });
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, crelue");
-  }
-
-  add = () => {
-    // setState: 새로운 상태를 객체로 지정해줌
-    // setState를 호출할 때마다 react는 새로운 state와 함꼐 ender function을 호출함
-    // this.setState({ count: this.state.cont + 1 });
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    // this.setState({ count: this.state.cont - 1 });
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
 
   render() {
-    console.log("Being Rendering");
-    return (
-      <div>
-        <h1> The number is : {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
